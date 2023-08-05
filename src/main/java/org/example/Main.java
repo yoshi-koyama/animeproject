59300a6607d1e56c715bc11a125a144d3662a7d5
yoshi-koyama <koyama@reytech.co.jp>
2023-08-05 20:37:07 +0900

    animeListにGintamaを追加

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> animeList = List.of("Naruto", "Bleach", "One Piece", "Dragon Ball");
+        List<String> animeList = List.of("Naruto", "Bleach", "One Piece", "Dragon Ball", "Gintama");
 
         animeList.forEach(System.out::println);
     }
